344c8b109845fea87ac15d647832a53073568e62
Daniel Wong <dsw2107@gmail.com>
2023-05-22 19:53:06 -0700

    Add num lock indicator, remove CG swap

--- keyboards/sofle_choc/keymaps/dswong/keymap.c
+++ keyboards/sofle_choc/keymaps/dswong/keymap.c
@@ -67,24 +67,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* LOWER
  * ,----------------------------------------.                    ,-----------------------------------------.
- * | ____ |  –   |  €   |  £   |  ¥   |      |                    | Ins  | Home | PgUp |  - _ | = +  | ____ |
+ * | ____ |  –   |  €   |  £   |  ¥   | Pscr |                    | Ins  | Home | PgUp |  - _ | = +  | ____ |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ____ |      | Pscr | Menu |      |      |                    | Del  | End  | PgDn | KP_7 | KP_8 | KP_9 |
+ * |  F4  |      |      |      |      | NumLk|                    | Del  | End  | PgDn | KP_7 | KP_8 | KP_9 |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ____ | LAlt | LCtl |LShift|      |      |-------.    ,-------|      |  Up  |      | KP_4 | KP_5 | KP_6 |
+ * | ____ |      |      |      |      |      |-------.    ,-------|   /  |  Up  |  *   | KP_4 | KP_5 | KP_6 |
  * |------+------+------+------+------+------| ____  |    | ____  |------+------+------+------+------+------|
  * | Caps | Undo |  Cut | Copy | Paste|      |-------|    |-------| Left | Down | Right| KP_1 | KP_2 | KP_3 |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | ____ | ____ | ____ |TO(0) | / ____  /       \ ____ \  |TO(3) | ____ | ] }  | KP_0 |
+ *            | ____ | ____ | ____ |TO(0) | / ____  /       \ ____ \  |TO(3) |  .   | ] }  | KP_0 |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 [_LOWER] = LAYOUT(
-    _______, KC_ENDASH, CS_EUR, CS_GBP, CS_JPY,   XXXXXXX,                         KC_INS,  KC_HOME, KC_PGUP, KC_MINUS,  KC_EQUAL, _______,
-    _______, XXXXXXX, KC_PSCR, KC_APP,  XXXXXXX,  XXXXXXX,                         KC_DEL,  KC_END,  KC_PGDN, KC_KP_7,   KC_KP_8,  KC_KP_9,
-    _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,  XXXXXXX,                         XXXXXXX, KC_UP,   XXXXXXX, KC_KP_4,   KC_KP_5,  KC_KP_6,
+    _______, KC_ENDASH, CS_EUR, CS_GBP, CS_JPY,   KC_PSCR,                         KC_INS,  KC_HOME, KC_PGUP, KC_MINUS,  KC_EQUAL, _______,
+    KC_F4,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_NUM,                          KC_DEL,  KC_END,  KC_PGDN, KC_KP_7,   KC_KP_8,  KC_KP_9,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                         KC_PSLS, KC_UP,   KC_PAST, KC_KP_4,   KC_KP_5,  KC_KP_6,
     KC_CAPS, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, XXXXXXX, _______,       _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_KP_1,   KC_KP_2,  KC_KP_3,
-                      _______, _______, _______,    TO(0), _______,       _______, TO(3),   _______, KC_RBRC, KC_KP_0
+                      _______, _______, _______,    TO(0), _______,       _______, TO(3),   KC_PDOT, KC_RBRC, KC_KP_0
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -94,9 +94,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | ____ |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
  * |------+------+------+------+------+------| ____  |    | ____  |------+------+------+------+------+------|
- * | ____ |  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  |      |
+ * |LShift|  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | ____ | ____ | ____ |TO(0) | / ____  /       \ ____ \  |TO(3) | ____ |      |      |
+ *            | ____ | ____ | ____ |TO(0) | / ____  /       \ ____ \  |TO(3) |RSHIFT|      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -104,8 +104,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     KC_GRV,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
     _______, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-    _______, KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, XXXXXXX,
-                     _______, _______, _______, TO(0),   _______,       _______, TO(3),   _______, XXXXXXX, XXXXXXX
+    KC_LSFT, KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, XXXXXXX,
+                     _______, _______, _______, TO(0),   _______,       _______, TO(3),   KC_RSFT, XXXXXXX, XXXXXXX
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -113,9 +113,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | BOOT | Spd+ | Val+ | Sat+ | Hue+ |Mode+ |                    |RGBInd|RGBVio|RGBWht|RGBGld|      |ComboT|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift| Spd- | Val- | Sat- | Hue- |Mode- |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
+ * | ____ | Spd- | Val- | Sat- | Hue- |Mode- |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
  * |------+------+------+------+------+------|RGBTogg|    |RL_Rset|------+------+------+------+------+------|
- * |CGTogg|Static|Breath|Snake |Knight|RL_CVG|-------|    |-------|      | PREV | PLAY | NEXT |      |      |
+ * | ____ |Static|Breath|Snake |Knight|RL_CVG|-------|    |-------|      | PREV | PLAY | NEXT |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | ____ | ____ | ____ |TO(0) | / ____  /       \ ____ \  |TO(0) | ____ |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -124,8 +124,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT(
     EE_CLR,  RGB_M_R, RGB_M_SW, RGB_M_G, RGB_M_X, RGB_M_TW,                     RL_RED,  RL_ORG,  RL_YEL,  RL_GRN,  RL_BLU,  AC_TOGG,
     QK_BOOT, RGB_SPI, RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD,                       RL_IND,  RL_VIO,  RL_WHT,  RL_GLD,  XXXXXXX, CM_TOGG,
-    KC_LSFT, RGB_SPD, RGB_VAD, RGB_SAD, RGB_HUD, RGB_RMOD,                      XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
-    CG_TOGG, RGB_M_P, RGB_M_B, RGB_M_SN, RGB_M_K, RL_CVGRN, RGB_TOG,   RL_RST,  XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
+    _______, RGB_SPD, RGB_VAD, RGB_SAD, RGB_HUD, RGB_RMOD,                      XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
+    _______, RGB_M_P, RGB_M_B, RGB_M_SN, RGB_M_K, RL_CVGRN, RGB_TOG,   RL_RST,  XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
                       _______, _______, _______, TO(0),   _______,     _______, TO(0),   _______, XXXXXXX, XXXXXXX
     )
 };
@@ -183,7 +183,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case RL_RST:
             if (record->event.pressed) {
                 rgblight_sethsv(HSV_WHITE);
-                rgblight_mode(9);
+                rgblight_mode(1);
                 rgblight_set_speed(0);
                 return false;
             }
@@ -226,13 +226,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case RL_IND: 
         if (record->event.pressed) {
-                rgblight_setrgb(0x4B, 0x00, 0x82);
+                rgblight_sethsv(174, 75, 100);
                 return false;
             }
             break;
         case RL_VIO: 
         if (record->event.pressed) {
-                rgblight_setrgb(0x8F, 0x00, 0xFF);
+                rgblight_sethsv(194, 100, 100);
                 return false;
             }
             break;
@@ -337,16 +337,21 @@ static void print_status_narrow(void) {
     // Show me the current status of CAPS LOCK and CG_SWAP on all layers
     led_t led_usb_state = host_keyboard_led_state();
     if (led_usb_state.caps_lock) {
-        oled_write_ln_P(PSTR(" CAP "), true);
+        oled_write_ln(PSTR(" CAP "), true);
+    } else {
+        oled_write_ln(PSTR("     "), false);
+    }
+    if (led_usb_state.num_lock) {
+        oled_write_ln_P(PSTR(" NUM "), false);
     } else {
         oled_write_ln_P(PSTR("     "), false);
     }
     
-    if (keymap_config.swap_lctl_lgui) {
-        oled_write_ln_P(PSTR("CG ←→"), true);
-    } else {
-        oled_write_ln_P(PSTR("     "), false);
-    }
+    // if (keymap_config.swap_lctl_lgui) {
+    //     oled_write_ln_P(PSTR(" CGx "), true);
+    // } else {
+    //     oled_write_ln_P(PSTR("     "), false);
+    // }
 
     // Print current layer
     switch (get_highest_layer(layer_state)) {
